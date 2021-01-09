4f774fb3313aa1b9afdcded771e7abc64add9c94
mike14747 <mike4747@oh.rr.com>
2021-01-08 22:34:52 -0500

    Updated config/connectionPool.js to accomodate the heroku deployment.

--- config/connectionPool.js
+++ config/connectionPool.js
@@ -1,16 +1,45 @@
 const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PW,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    multipleStatements: true,
-});
+let pool;
+
+if (process.env.JAWSDB_URL) {
+    const url = new URL(process.env.JAWSDB_URL);
+    pool = mysql.createPool({
+        host: url.hostname,
+        port: url.port,
+        user: url.username,
+        password: url.password,
+        database: url.pathname.replace('/', ''),
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0,
+        multipleStatements: true,
+    });
+} else {
+    pool = mysql.createPool({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PW,
+        database: process.env.DB_NAME,
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0,
+        multipleStatements: true,
+    });
+}
+
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PW,
+//     database: process.env.DB_NAME,
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0,
+//     multipleStatements: true,
+// });
 
 const dbTest = () => {
     return new Promise((resolve, reject) => {
