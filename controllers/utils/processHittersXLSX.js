5133e8cf04aed0bd8fa3e024528db3aea0f68ae1
mike14747 <mgullo.dev@gmail.com>
2024-01-23 00:53:43 -0500

    Added processHittersXLSX.js and installed the exceljs package.

---
+++ controllers/utils/processHittersXLSX.js
@@ -0,0 +1,13 @@
+// const ExcelJS = require('exceljs');
+
+/*
+I'm going to try to make this script process hitter data which is in .xlsx format instead of .csv.
+
+That way I won't need to keep the file in both formats. All I'll need is the .xlsx file.
+
+These seem to be the npm packages I could use (in no special order):
+
+1. exceljs (seems to be the leading candidate)
+2. node-xlsx
+3. xlsx
+*/
