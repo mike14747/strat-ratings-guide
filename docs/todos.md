dab4ce1ee95cc7efffc993c93c4a3e08e7f9cb96
mike14747 <mgullo.dev@gmail.com>
2024-01-28 14:19:30 -0500

    Began styling and implementing the season-dropdown-component. Removed a couple unused npm packages.

--- docs/todos.md
+++ docs/todos.md
@@ -6,7 +6,7 @@ Add season list dropdowns to the hitter and pitcher analysis pages.
 
 ~~Convert all colors in globals.css to css variables.~~
 
-Figure out how to have a button to copy all hitter and pitcher data. UPDATE: it's been implemented successfully for the hitters page.
+~~Figure out how to have a button to copy all hitter and pitcher data.~~
 
 Figure out how to upload/process .xlsx files instead of .csv files.
 
--- frontend/components/seasonDropdown.js
+++ frontend/components/seasonDropdown.js
@@ -2,7 +2,23 @@ class SeasonDropdownComponent extends HTMLElement {
     constructor() {
         super();
         this.innerHTML = `
-        
+        <nav class="sd">
+            <div class="button-container"}>
+                <button>
+                    <span class="menu-mutton">Seasons</span>
+                </button>
+            </div>
+
+            <ul class="sd-dropdown-content">
+                <li>
+                    <a href="/hitter-analysis?season=2023">2023</a>
+                </li>
+
+                <li>
+                    <a href="/hitter-analysis?season=2022">2022</a>
+                </li>
+            </ul >
+        </nav >
         `;
     }
 }
