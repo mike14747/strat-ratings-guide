02aaafe67f71d658ca3d378ea999de552eb47d85
mike14747 <mgullo.dev@gmail.com>
2024-01-29 00:47:46 -0500

    Removed the outer div from the season dropdown web component.

--- frontend/components/seasonDropdown.js
+++ frontend/components/seasonDropdown.js
@@ -2,18 +2,16 @@ class SeasonDropdownComponent extends HTMLElement {
     constructor() {
         super();
         this.innerHTML = `
-        <div>
-            <div class="dropdown">
-                <button class="dropbtn">Season<i class="down"></i></button>
-                <ul class="dropdown-content">
-                    <a href="/hitter-analysis?season=2023"><li>2023</li></a>
-                    <a href="/hitter-analysis?season=2022"><li>2022</li></a>
-                    <a href="/hitter-analysis?season=2021"><li>2021</li></a>
-                    <a href="/hitter-analysis?season=2020"><li>2020</li></a>
-                    <a href="/hitter-analysis?season=2019"><li>2019</li></a>
-                    <a href="/hitter-analysis?season=2018"><li>2018</li></a>
-                </ul>
-            </div>
+        <div class="dropdown">
+            <button class="dropbtn">Season<i class="down"></i></button>
+            <ul class="dropdown-content">
+                <a href="/hitter-analysis?season=2023"><li>2023</li></a>
+                <a href="/hitter-analysis?season=2022"><li>2022</li></a>
+                <a href="/hitter-analysis?season=2021"><li>2021</li></a>
+                <a href="/hitter-analysis?season=2020"><li>2020</li></a>
+                <a href="/hitter-analysis?season=2019"><li>2019</li></a>
+                <a href="/hitter-analysis?season=2018"><li>2018</li></a>
+            </ul>
         </div>
         `;
     }
