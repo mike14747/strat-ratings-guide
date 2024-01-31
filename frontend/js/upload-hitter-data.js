9bc6823ebce5a04dd7d62651ed1dd6bfc49f01a4
mike14747 <mgullo.dev@gmail.com>
2024-01-31 01:11:20 -0500

    Started trying to get hitter_ratings.csv uploaded to the server.

--- frontend/js/upload-hitter-data.js
+++ frontend/js/upload-hitter-data.js
@@ -1,1 +1,30 @@
-// upload-hitter-data
+async function uploadFile() {
+    const fileUpload = document.getElementById('file-input');
+    const file = fileUpload.files[0];
+
+    // make sure the file is of the correct name and type
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    const url = '/api/hitters';
+    const dataJSON = await fetch(url, {
+        method: 'POST',
+        body: formData,
+        headers: {
+            'Content-Type': 'multipart/form-data',
+        },
+    }).then(res => res.json().catch(error => console.error(error)));
+
+    if (dataJSON) {
+        document.getElementById('message').innerHTML = `<p class="success">${dataJSON.message}</p>`;
+        return;
+    }
+
+    document.getElementById('message').innerHTML = `<p class="danger">File upload failed!</p>`;
+}
+
+const fileUploadForm = document.getElementById('file-upload-form');
+fileUploadForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+});
