300b4ce6337b9fb5b96a1bbe15375978c04ab355
mike14747 <mgullo.dev@gmail.com>
2025-02-02 23:11:51 -0500

    Updated a lot of values in fiedingWopsCalculate.ts.

--- controllers/utils/fieldingWopsCalculate.ts
+++ controllers/utils/fieldingWopsCalculate.ts
@@ -21,47 +21,47 @@ type Positions = {
 };
 
 const catcherRangeRatings: PositionRangeRatings = {
-    1: { si: 0, do: 0, tr: 0, gba: 0 },
-    2: { si: 0, do: 0, tr: 0, gba: 0 },
+    1: { si: 0, do: 0, tr: 0, gba: 0.10 },
+    2: { si: 0, do: 0, tr: 0, gba: 0.05 },
     3: { si: 0, do: 0, tr: 0, gba: 0 },
     4: { si: 0, do: 0, tr: 0, gba: 0 },
     5: { si: 0, do: 0, tr: 0, gba: 0 },
 };
 
 const infieldRangeRatings: PositionRangeRatings = {
-    1: { si: 0, do: 0, tr: 0, gba: 0 },
-    2: { si: 0, do: 0, tr: 0, gba: 0 },
-    3: { si: 0, do: 0, tr: 0, gba: 0 },
-    4: { si: 0, do: 0, tr: 0, gba: 0 },
-    5: { si: 0, do: 0, tr: 0, gba: 0 },
+    1: { si: 0, do: 0, tr: 0, gba: 0.65 },
+    2: { si: 0.10, do: 0, tr: 0, gba: 0.45 },
+    3: { si: 0.20, do: 0, tr: 0, gba: 0.25 },
+    4: { si: 0.30, do: 0, tr: 0, gba: 0.15 },
+    5: { si: 0.40, do: 0, tr: 0, gba: 0.05 },
 };
 
 const ofRangeRatings: PositionRangeRatings = {
     1: { si: 0, do: 0, tr: 0, gba: 0 },
-    2: { si: 0, do: 0, tr: 0, gba: 0 },
-    3: { si: 0, do: 0, tr: 0, gba: 0 },
-    4: { si: 0, do: 0, tr: 0, gba: 0 },
-    5: { si: 0, do: 0, tr: 0, gba: 0 },
+    2: { si: 0.10, do: 0.05, tr: 0, gba: 0 },
+    3: { si: 0.15, do: 0.15, tr: 0, gba: 0 },
+    4: { si: 0.35, do: 0.15, tr: 0.05, gba: 0 },
+    5: { si: 0.40, do: 0.25, tr: 0.10, gba: 0 },
 };
 
 const pitcherRangeRatings: PositionRangeRatings = {
-    1: { si: 0, do: 0, tr: 0, gba: 0 },
-    2: { si: 0, do: 0, tr: 0, gba: 0 },
-    3: { si: 0, do: 0, tr: 0, gba: 0 },
-    4: { si: 0, do: 0, tr: 0, gba: 0 },
-    5: { si: 0, do: 0, tr: 0, gba: 0 },
+    1: { si: 0, do: 0, tr: 0, gba: 0.25 },
+    2: { si: 0.10, do: 0, tr: 0, gba: 0.20 },
+    3: { si: 0.15, do: 0, tr: 0, gba: 0.15 },
+    4: { si: 0.20, do: 0, tr: 0, gba: 0.10 },
+    5: { si: 0.30, do: 0, tr: 0, gba: 0.05 },
 };
 
 const positions: Positions = {
     CA: { chances: 3, errorFactor: 0.0120, errorTypeRates: { e1: 0.9539, e2: 0.0461, e3: 0 }, rangeRatings: catcherRangeRatings },
-    '1B': { chances: 2, errorFactor: 0.0180, errorTypeRates: { e1: 0, e2: 0, e3: 0 }, rangeRatings: infieldRangeRatings },
-    '2B': { chances: 6, errorFactor: 0.0060, errorTypeRates: { e1: 0, e2: 0, e3: 0 }, rangeRatings: infieldRangeRatings },
-    '3B': { chances: 3, errorFactor: 0.0120, errorTypeRates: { e1: 0, e2: 0, e3: 0 }, rangeRatings: infieldRangeRatings },
-    SS: { chances: 7, errorFactor: 0.0052, errorTypeRates: { e1: 0, e2: 0, e3: 0 }, rangeRatings: infieldRangeRatings },
-    LF: { chances: 2, errorFactor: 0.0180, errorTypeRates: { e1: 0, e2: 0, e3: 0 }, rangeRatings: ofRangeRatings },
-    CF: { chances: 3, errorFactor: 0.0120, errorTypeRates: { e1: 0, e2: 0, e3: 0 }, rangeRatings: ofRangeRatings },
-    RF: { chances: 2, errorFactor: 0.0180, errorTypeRates: { e1: 0, e2: 0, e3: 0 }, rangeRatings: ofRangeRatings },
-    P: { chances: 2, errorFactor: 0.0180, errorTypeRates: { e1: 0, e2: 0, e3: 0 }, rangeRatings: pitcherRangeRatings },
+    '1B': { chances: 2, errorFactor: 0.0180, errorTypeRates: { e1: 0.9519, e2: 0.0481, e3: 0 }, rangeRatings: infieldRangeRatings },
+    '2B': { chances: 6, errorFactor: 0.0060, errorTypeRates: { e1: 0.8969, e2: 0.1031, e3: 0 }, rangeRatings: infieldRangeRatings },
+    '3B': { chances: 3, errorFactor: 0.0120, errorTypeRates: { e1: 0.8003, e2: 0.1997, e3: 0 }, rangeRatings: infieldRangeRatings },
+    SS: { chances: 7, errorFactor: 0.0052, errorTypeRates: { e1: 0.8500, e2: 0.1500, e3: 0 }, rangeRatings: infieldRangeRatings },
+    LF: { chances: 2, errorFactor: 0.0180, errorTypeRates: { e1: 0.2015, e2: 0.7538, e3: 0.0508 }, rangeRatings: ofRangeRatings },
+    CF: { chances: 3, errorFactor: 0.0120, errorTypeRates: { e1: 0.2080, e2: 0.7428, e3: 0.0537 }, rangeRatings: ofRangeRatings },
+    RF: { chances: 2, errorFactor: 0.0180, errorTypeRates: { e1: 0.2015, e2: 0.7538, e3: 0.0508 }, rangeRatings: ofRangeRatings },
+    P: { chances: 2, errorFactor: 0.0180, errorTypeRates: { e1: 0.9486, e2: 0.0514, e3: 0 }, rangeRatings: pitcherRangeRatings },
 };
 
 console.log({ positions });
